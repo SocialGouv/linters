9f0afcc0a2d306e502f1dceb7a2f65c97fd917db
Douglas Duteil <douglasduteil@gmail.com>
2018-10-16 17:44:30 +0200

    style(recommended): reorder env keys

--- packages/eslint-config-recommended/index.js
+++ packages/eslint-config-recommended/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   env: {
     browser: true,
-    node: true,
     commonjs: true,
-    es6: true
+    es6: true,
+    node: true
   },
   parserOptions: {
     ecmaVersion: 2018,
