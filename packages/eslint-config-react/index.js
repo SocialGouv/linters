f696cd547016bf8621b6897c06ca066876a03f9b
kwiss <christophe@hooraylabs.com>
2019-08-21 23:24:52 +0200

    feat(react-linter): extend config with sort import alphabet keys and better prettier integration

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,15 +1,33 @@
 module.exports = {
   extends: [
     "@socialgouv/eslint-config-recommended",
+    "airbnb",
     "plugin:jsx-a11y/recommended",
     "plugin:react/recommended",
-    "prettier/react"
+    "prettier",
+    "prettier/react",
+    "plugin:import/errors",
+    "plugin:import/warnings"
   ],
   parser: "babel-eslint",
-  plugins: ["jsx-a11y", "react", "react-hooks"],
+  plugins: [
+    "jsx-a11y",
+    "react",
+    "react-hooks",
+    "sort-keys-fix",
+    "simple-import-sort",
+    "prettier",
+    "import",
+    "jest",
+    "json",
+    "html"
+  ],
   rules: {
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+    "simple-import-sort/sort": error,
+    "sort-keys-fix/sort-keys-fix": [error, "asc"],
+    "import/prefer-default-export": ignore
   },
   settings: {
     react: {
