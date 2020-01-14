086bf4911ac2590aa873e50f3aec85149f6d0bce
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2020-01-14 11:57:08 +0100

    feat(eslint-config-strict): create (#307)

---
+++ packages/eslint-config-strict/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ["@socialgouv/eslint-config-recommended"],
+  parser: "babel-eslint",
+  plugins: [
+    // Sort imports:
+    "simple-import-sort",
+    // Sort keys in objects:
+    "sort-keys-fix"
+  ],
+  rules: {
+    // Force imports sorting:
+    "simple-import-sort/sort": "error",
+    // Force keys sorting in objects:
+    "sort-keys-fix/sort-keys-fix": "error"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+};
