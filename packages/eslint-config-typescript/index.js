b18b4614c978e07f8df94703f1ddb52a00ac23e1
Douglas Duteil <douglasduteil@gmail.com>
2020-03-23 13:37:47 +0100

    feat(eslint-config-typescript)!: migrate our tslint config
    BREAKING CHANGE: remove `@socialgouv/tslint-config-recommended`
        - As tslint is deprecated now
          https://github.com/palantir/tslint/commit/119458e17c5bd37ca84b6d7a7a4295649d2103c0

---
+++ packages/eslint-config-typescript/index.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: [
+    "@socialgouv/eslint-config-recommended",
+    //
+    "plugin:@typescript-eslint/all",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:import/typescript",
+    //
+    "prettier/@typescript-eslint",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: "tsconfig.json" },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-type-alias": "off",
+    "@typescript-eslint/prefer-readonly-parameter-types": "off",
+    "@typescript-eslint/typedef": "off",
+    "import/order": [
+      "error",
+      {
+        alphabetize: { order: "asc", caseInsensitive: true },
+        "newlines-between": "always",
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: "jest.config.js",
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
+};
