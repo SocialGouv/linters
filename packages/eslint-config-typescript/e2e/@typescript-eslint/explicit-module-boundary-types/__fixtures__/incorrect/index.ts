a7994b2b4dc54333833ece2dbd67194e43e0ae75
Douglas Duteil <douglasduteil@gmail.com>
2020-05-27 17:58:33 +0200

    test(typescrpt): remove similar tests

--- packages/eslint-config-typescript/e2e/@typescript-eslint/explicit-module-boundary-types/__fixtures__/incorrect/index.ts
+++ packages/eslint-config-typescript/e2e/@typescript-eslint/explicit-module-boundary-types/__fixtures__/incorrect/index.ts
@@ -10,11 +10,8 @@ export default function () {
 // Should indicate that a string is returned
 export const arrowFn1 = () => "test";
 
-// All arguments should be typed
-export const arrowFn2 = (arg): string => `test ${arg}`;
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const arrowFn3 = (arg: any): string => `test ${arg as string}`;
+export const arrowFn2 = (arg: any): string => `test ${arg as string}`;
 
 export class Test {
   // Should indicate that no value is returned (void)
