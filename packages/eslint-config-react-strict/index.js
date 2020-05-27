7c57cc5a75c155ce6bb85dddedf4e0dc846a96fd
Douglas Duteil <douglasduteil@gmail.com>
2020-05-27 14:12:31 +0200

    styles: self lint each pacakge

--- packages/eslint-config-react-strict/index.js
+++ packages/eslint-config-react-strict/index.js
@@ -8,12 +8,14 @@ module.exports = {
   parser: "babel-eslint",
   plugins: ["jsx-a11y", "react", "react-hooks"],
   rules: {
-    // Prevent extra closing tags for components without children:
-    "react/self-closing-comp": ["error", { component: true, html: true }],
-    // Enforce React official rules for hooks:
-    "react-hooks/rules-of-hooks": "error",
     // Force exhaustive dependencies injection within hooks:
     "react-hooks/exhaustive-deps": "error",
+
+    // Enforce React official rules for hooks:
+    "react-hooks/rules-of-hooks": "error",
+
+    // Prevent extra closing tags for components without children:
+    "react/self-closing-comp": ["error", { component: true, html: true }],
   },
   settings: {
     react: {
