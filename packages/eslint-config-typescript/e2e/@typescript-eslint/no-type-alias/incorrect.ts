9da6ad8555323d8edfad69b204f6590c1dd58ee0
Douglas DUTEIL <douglasduteil@gmail.com>
2021-01-28 19:09:40 +0100

    update to eslint monorepo config

--- packages/eslint-config-typescript/e2e/@typescript-eslint/no-type-alias/incorrect.ts
+++ packages/eslint-config-typescript/e2e/@typescript-eslint/no-type-alias/incorrect.ts
@@ -1,16 +1,3 @@
 /* eslint-disable @typescript-eslint/no-extraneous-class */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
-type Foou = "a";
-
-type Foouo = string;
-
-type Foouoo = string & string[];
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Bar {}
-class Baz implements Bar {}
-
-type Foouooo = Bar;
-
-type Foouoooo = Bar & Baz;
+type Foo = keyof [string] | unique symbol;
