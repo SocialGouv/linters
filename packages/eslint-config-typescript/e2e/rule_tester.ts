6ade64d9f848807599381a297e87b4573bae0e69
Douglas Duteil <douglasduteil@gmail.com>
2021-05-25 18:02:10 +0200

    test: CLIEngine class has been deprecated (#666)

--- packages/eslint-config-typescript/e2e/rule_tester.ts
+++ packages/eslint-config-typescript/e2e/rule_tester.ts
@@ -1,25 +1,25 @@
-import { CLIEngine } from "eslint";
+import { ESLint } from "eslint";
 import { readFile } from "fs/promises";
 import { basename, dirname, relative, resolve } from "path";
 
-export const cli = new CLIEngine({
-  configFile: "./index.js",
+export const cli = new ESLint({
   cwd: dirname(__dirname),
   ignore: false,
+  overrideConfigFile: "./index.js",
   useEslintrc: false,
 });
 export function ruleTest(file: string): () => Promise<void> {
   const filePath = resolve(dirname(file), basename(file, ".test.ts") + ".ts");
   const fileContent = readFile(filePath, "utf8");
   return async () => {
-    expect(cli.executeOnText(await fileContent, filePath)).toMatchSnapshot({
-      results: [
-        {
-          filePath: expect.stringContaining(
-            relative(dirname(__dirname), filePath)
-          ),
-        },
-      ],
-    });
+    await expect(
+      cli.lintText(await fileContent, { filePath })
+    ).resolves.toMatchSnapshot([
+      {
+        filePath: expect.stringContaining(
+          relative(dirname(__dirname), filePath)
+        ),
+      },
+    ]);
   };
 }
